1c3933d08fd8892676fc2c80b65a0f934c756295
danni <594danielwu@gmail.com>
2025-01-20 14:51:18 +0800

    init commit

---
+++ LazyBuster/LazyBusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyBusterApp.swift
+//  LazyBuster
+//
+//  Created by Daniel on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct LazyBusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
